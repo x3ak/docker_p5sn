f5f8632e6f61e1b470b73f8ffaa3cfc77debeb27
Pavel Galaton <pgalaton@pentalog.fr>
2016-08-22 17:21:29 +0300

    Extra hosts and zsh

--- enter-container.sh
+++ enter-container.sh
@@ -3,4 +3,4 @@
 username=${1-root}
 container=${2-p5sn}
 
-docker exec -it -u ${username} ${container} /bin/bash -c "export TERM=xterm; exec bash"
+docker exec -it -u ${username} ${container} /bin/bash -c "export TERM=xterm; exec zsh"
