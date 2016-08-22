80543d8de8ef216da0d33a095454d265249ff902
Marius Tamazlicaru <mtamazlicaru@pentalog.fr>
2016-08-22 19:25:58 +0300

    Changes for enter-container.sh due can enter and other container not only p5sn. Adjustment in app.conf with ProcyPassMatch.

--- enter-container.sh
+++ enter-container.sh
@@ -3,4 +3,8 @@
 username=${1-root}
 container=${2-p5sn}
 
-docker exec -it -u ${username} ${container} /bin/bash -c "export TERM=xterm; exec zsh"
+if [ ${container} = "p5sn" ]; then
+   docker exec -it -u ${username} ${container} /bin/bash -c "export TERM=xterm; exec zsh"
+else
+   docker exec -it -u ${username} ${container} /bin/bash -c "export TERM=xterm; exec bash"
+fi
